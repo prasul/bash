8b7ad3aeb9e7d8e164795fe376dc241327bc4604
Prasul Surendran <36921873+prasul@users.noreply.github.com>
2020-10-10 13:19:46 +0530

    Update sharexformac.sh
    Added image optimization -
    added few more logic to the simple script, still unfinished.

--- sharexformac.sh
+++ sharexformac.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-#version 0.3
+#version 0.4
 #Prasuls bash script to create a random name for screenshot name in MAC and upload it to FTP server. 
 #Upload the script on to the folder where the screenshots are getting created.
 #Warning: Script doesn't take screenshots yet - it just renames and uploads the file to FTP
@@ -18,26 +18,38 @@ scrnshotprfx=
 for f in *\ *; do mv 2>/dev/null "$f" "${f// /_}"; done
 
 #filename change and upload
-for file in `find . -name "$scrnshotprfx*.png"`;
+for file in `find . -name "$scrnshotprfx*"`;
 do 
 	ext=${file: -4};
+	echo $ext;
+	if [[ "$ext" != *"."* ]];
+	then
+		echo "$file does not a valid screenshot extension";
+		continue;
+	elif [ $ext == ".png" ]
+	then
+		echo "compressing the file.."
+		magick $file -strip -interlace Plane -resize 80% -quality 80 newfile.png || { echo 'convert command failed, check if imagemagick is installed' ; exit 1; }
+		rm -fv $file;
+		file=newfile.png;
+	else
+		echo "no optimizations done!"
+	fi
 	filename=`/usr/bin/openssl rand -hex 24`;
-  	mv "$file" $filename$ext;
-	tput setaf 2;
-  	echo "\nUploading screenshot:";tput setaf 3; 
-	curl --progress-bar -T $filename$ext ftp://$ftpservip --user $ftpun:$ftppw; 
+	mv "$file" $filename$ext;
+	tput setaf 2;echo "\nUploading screenshot:";tput setaf 3; 
+	curl --progress-bar -T $filename$ext ftp://$ftpservip --user $ftpun:$ftppw || { echo 'uploading failed - try again manually' ; exit 1; }
 	newshareurl="$shareurl/$filename$ext"; 
 	echo $newshareurl >> urls;
-	tput setaf 4; echo "removing screenshot from local...";
+	newshareurl="";
+	tput setaf 4; echo "removing screenshot...";
 	rm -v $filename*
 done
 
 #beautify the result
 tput setaf 2; echo  "=======================\n Share URL \n======================="
 tput sgr0;
-cat urls;
-
-#copy the url to clipboard
+cat 2>/dev/null urls|| { echo 'No urls to share :(' ; exit 1; }
 echo "\n(URL copied!)"
 cat urls|pbcopy
 rm urls;
